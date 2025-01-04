ea6979d60b3dd72d218a700e8b311f48ad2637ea
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-04 17:08:41 +0200

    +

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 "use strict";
 
+// ---------- MENU ----------
+
+const menuToggler = document.querySelector('.menu-toggler');
+// const menuTogglerIcon = document.querySelector('.menu-toggler__icon');
+
+menuToggler.onclick = function () {
+    menuToggler.classList.toggle('menu-toggler--active');
+    document.body.classList.toggle('no-scroll');
+};
+
 // ---------- MODAL ----------
 
 const modal = new GraphModal();
