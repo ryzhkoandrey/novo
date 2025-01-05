f86b8ca32d9462c832d7882c12776318fc2b16fb
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-05 20:44:11 +0200

    +

--- js/main.js
+++ js/main.js
@@ -9,6 +9,43 @@ menuToggler.onclick = function () {
     document.body.classList.toggle('no-scroll');
 };
 
+// ---------- ABOUT ----------
+
+// swiper
+
+let swiper;
+const pageWidht = 478;
+let isSwiperActive = false;
+
+function initSwiper() {
+    if (window.innerWidth <= pageWidht && !swiper) {
+        swiper = new Swiper('.swiper', {
+            slidesPerView: 'auto',
+            spaceBetween: 10,
+            freeMode: true,
+        });
+        isSwiperActive = true;
+    }
+}
+
+function destroySwiper() {
+    if (window.innerWidth > pageWidht && swiper) {
+        swiper.destroy(true, true);
+        swiper = null;
+        isSwiperActive = false;
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= pageWidht && !isSwiperActive) {
+        initSwiper();
+    } else if (window.innerWidth > pageWidht && isSwiperActive) {
+        destroySwiper();
+    }
+});
+
+initSwiper();
+
 // ---------- MODAL ----------
 
 const modal = new GraphModal();
